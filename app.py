85db8f218072d93a02f3bfe6cc5f1f9c26e44081
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-03-10 14:20:27 -0300

    Fazendo

--- app.py
+++ app.py
@@ -34,3 +34,44 @@ class OrdemVendas(BaseModel):
     id: int
     cliente_id: int
     produto_id: int
+
+clientes_id = []
+
+
+@app.post("/add_cliente")
+async def add_cliente(cliente:Cliente):
+    data = [
+        ["ID", "NOME", "SOBRENOME", "DATA", "CPF"]
+    ]
+
+    Clientes = {}
+    ultimo_id = clientes_id[-1] + 1
+
+    if not clientes_id:
+        ultimo_id = 1
+        clientes_id.append(ultimo_id)
+
+    with open(funcoes.cliente_csv, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == 'ID':
+                continue
+            else:
+                data.append(row)
+
+    novo = [cliente.id, cliente.nome]
+    data.append(novo)
+
+    with open(funcoes.cliente_csv, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+    
+    with open(funcoes.cliente_csv, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == 'ID':
+                continue
+            else:
+                Clientes[row[0]] = row[1]
+
+    return Clientes
